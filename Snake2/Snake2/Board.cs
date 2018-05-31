3c534ad676213efb7217e80bf6414dd2f4c6d097
Zack Jones <justzackj@gmail.com>
2018-05-31 09:11:35 -0600

    Added Snake2
    Added Snake2.
    Snake two is a more object oriented snake build which is only 261 lines of original code. I could probably get it down a bit with some more optimization but im happy with where it is.
    
    Interesting thing i learned in this project was that the snake is very similar to a queue, as long as you drop the notion of a static head and tail. Using a queue, i only need to add or delete a maximum of two snake parts(+head -tail) at any given interval.

---
+++ Snake2/Snake2/Board.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Snake2
+{
+    class Board
+    {
+        Snake s;
+
+        List<Point> mice;
+
+        int width;
+        int height;
+
+        public Board() { 
+            Console.CursorVisible = false;
+            Console.CursorSize = 100;
+        }
+
+        public void start() {
+            width = Console.WindowWidth;
+            height = Console.WindowHeight - 2;
+            s = new Snake(width / 2, height / 2);
+            mice = new List<Point>(2);
+            drawBoard();
+            loop();
+        }
+
+        private void drawBoard() {
+            Console.Clear();
+
+            ConsoleColor[] prev = prevCC();
+            setCC(ConsoleColor.White);
+
+            for (int i = 0; i < width; i++) {
+                writeCP(i, 0);
+                writeCP(i, height - 1);
+            }
+            for (int i = 0; i < height; i++) {
+                writeCP(0, i);
+                writeCP(width - 1, i);
+            }
+            setCC(prev);
+        }
+
+        #region Console Utility Methods
+        private ConsoleColor[] prevCC() {
+            return new ConsoleColor[2]{ Console.ForegroundColor, Console.BackgroundColor };
+        }
+        private void setCC(ConsoleColor cc) {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = cc;
+        }
+        private void setCC(ConsoleColor[] cc) {
+            Console.ForegroundColor = cc[0];
+            Console.BackgroundColor = cc[1];
+        }
+        private void setCC(ConsoleColor cc_f, ConsoleColor cc_b){
+            Console.ForegroundColor = cc_f;
+            Console.BackgroundColor = cc_b;
+        }
+        private void writeCP(int left, int top, char c = '0') {
+            Console.CursorLeft = left;
+            Console.CursorTop = top;
+            Console.Write(c);
+        }
+        #endregion
+
+        private void message(string message) {
+            ConsoleColor[] prev = prevCC();
+            setCC(ConsoleColor.Black);
+
+            for (int i = 0; i < width; i++) writeCP(i, height);
+            setCC(ConsoleColor.White, ConsoleColor.Black);
+            Console.SetCursorPosition(0, height);
+            Console.Write(message);
+            setCC(prev);
+        }
+
+        private void addMouse() {
+            Random r = new Random();
+            int x = 0, y = 0;
+            bool works;
+            do {
+                works = true;
+                x = r.Next(2, width - 3);
+                y = r.Next(2, height - 4);
+                foreach (Point p in s.snake)
+                    if (x == p.x && y == p.y) {
+                        works = false;
+                    }
+
+            } while (!works);
+
+            ConsoleColor[] prev = prevCC();
+            setCC(ConsoleColor.Red);
+            mice.Add(new Point(x, y));
+            writeCP(mice.Last().x, mice.Last().y);
+            setCC(prev);
+        }
+
+        private void reset(string s_message) {
+            message(s_message + " Your final score was: " + s.length);
+            while (Console.ReadKey().Key != ConsoleKey.R) ;
+            this.start();
+        }
+
+        private void loop() {
+            uint prevS = s.length;
+            while (true) {
+                if (Console.KeyAvailable) {
+                    ConsoleKey key = Console.ReadKey(false).Key;
+                    Direction dnew = Direction.UP;
+                    switch (key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            dnew = Direction.UP;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            dnew = Direction.RIGHT;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            dnew = Direction.DOWN;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            dnew = Direction.LEFT;
+                            break;
+                        default: break;
+                    }
+
+                    if ((dnew == Direction.UP && s.direction == Direction.DOWN)
+                    || (dnew == Direction.DOWN && s.direction == Direction.UP)
+                    || (dnew == Direction.LEFT && s.direction == Direction.RIGHT)
+                    || (dnew == Direction.RIGHT && s.direction == Direction.LEFT));
+                    else s.direction = dnew;
+                }
+                if (prevS != s.length) {
+                    prevS = s.length;
+                    message("Score: " + s.length);
+                }
+
+                ConsoleColor[] prev = prevCC();
+
+                Point tail = s.move();
+                if (tail != null){
+                    setCC(ConsoleColor.Black);
+                    writeCP(tail.x, tail.y);
+                }
+
+                setCC(ConsoleColor.Green);
+                Point head = s.head;
+                writeCP(head.x, head.y);
+                setCC(prev);
+
+                for (int i = 0; i < mice.Count; i++)
+                    if (head.x == mice[i].x && head.y == mice[i].y){
+                        s.addLength(2);
+                        mice.RemoveAt(i);
+                    }
+
+                if (mice.Count < 3) addMouse();
+
+                if (s.head.x <= 1 || s.head.x >= width - 2
+                 || s.head.y <= 1 || s.head.y >= height - 2) reset("You hit the wall!");
+                else if (s.selfCollision)                    reset("You ran into your self!");
+
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
---
+++ Snake2/Snake2/Snake.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake2
+{
+    class Snake
+    {
+
+        public uint length { get; private set; }
+        public uint maxlength { get; private set; }
+        public bool isGrowing { get; private set; }
+
+        public Direction direction { get; set; }
+
+        public Queue<Point> snake;
+        public Point head;
+
+        public bool selfCollision { get; private set; }
+
+        public Snake(int startx, int starty, uint startlength = 5, Direction starting_direction = Direction.LEFT) {
+            length = 0;
+            maxlength = startlength;
+            direction = starting_direction;
+            snake = new Queue<Point>(10);
+            snake.Enqueue(new Point(startx, starty));
+            head = snake.First();
+        }
+
+        public Point move() {
+            if (length < maxlength) isGrowing = true;
+            else isGrowing = false;
+
+            Point pnew = new Point(snake.Last().x, snake.Last().y);
+
+            switch (direction) {
+                case Direction.UP:
+                    pnew.y--;
+                    break;
+                case Direction.RIGHT:
+                    pnew.x++;
+                    break;
+                case Direction.DOWN:
+                    pnew.y++;
+                    break;
+                case Direction.LEFT:
+                    pnew.x--;
+                    break;
+            }
+
+            foreach (Point p in snake)
+                if (p.x == pnew.x && p.y == pnew.y)
+                    selfCollision = true;
+            snake.Enqueue(pnew);
+            head = pnew;
+            if (isGrowing) {
+                length++;
+                return null;
+            } else return snake.Dequeue();
+        }
+
+        public void addLength(uint amount) {
+            maxlength += amount;
+        }
+    }
+
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+    public enum Direction { UP, RIGHT, LEFT, DOWN }
+}
